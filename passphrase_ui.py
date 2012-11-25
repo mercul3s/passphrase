a4f921716d07fe6d969f46c0913d035ebd0c8938
mercul3s <mercul3s@gmail.com>
2012-11-25 14:17:05 -0800

    added functioning radio buttons for slang choice and input field for passphrase length

--- passphrase_ui.py
+++ passphrase_ui.py
@@ -1,20 +1,30 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, flash
 import passphrase_functions
 
 app = Flask(__name__)
 
 # display the home page on
-@app.route("/")
+@app.route("/", methods=["GET"])
 def index():
 	return render_template('passphrase.html')
 
-@app.route("/enter_length", methods=["POST"])
+@app.route("/passphrase", methods=["POST"])
 def gen_pass():
-	word_list = passphrase_functions.open_file('415words.txt')
-	pass_len = int(request.form['length'])
-	#language = request.form['']
-	passphrase = passphrase_functions.gen_pw(pass_len, word_list)
-	return passphrase
+	pass_len = request.form['pass-length']
+	slang = request.form['slang']
+	print pass_len
+	print slang
+	# if pass_len == '':
+	# and slang == None:
+	# 	pass_len = 3
+	# 	slang = '415words.txt'
+	word_list = passphrase_functions.open_file(slang)
+	passphrase = passphrase_functions.gen_pw(int(pass_len), word_list)
+	flash(passphrase)
+	return redirect('/')
+
+app.secret_key = 'TheStick?Hubba#Penelopes$'
 
 if __name__ == "__main__":
-    app.run(debug=True)
+	app.debug = True
+	app.run(host='localhost', port=5000)
