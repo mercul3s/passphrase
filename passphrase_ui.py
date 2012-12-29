9b9c9cc1e8b3bc16546481304c74905c4a3a4df7
mercul3s <mercul3s@gmail.com>
2012-12-29 12:50:28 -0800

    added try/except for KeyError to default to 3 word length and 415 slang dict for password generation if values are not submitted in the form.

--- passphrase_ui.py
+++ passphrase_ui.py
@@ -10,18 +10,26 @@ def index():
 
 @app.route("/passphrase", methods=["POST"])
 def gen_pass():
-	pass_len = request.form['pass-length']
-	slang = request.form['slang']
-	print pass_len
-	print slang
-	# if pass_len == '':
-	# and slang == None:
-	# 	pass_len = 3
-	# 	slang = '415words.txt'
-	word_list = passphrase_functions.open_file(slang)
-	passphrase = passphrase_functions.gen_pw(int(pass_len), word_list)
-	flash(passphrase)
-	return redirect('/')
+	try:
+		pass_len = request.form['pass-length']
+		slang = request.form['slang']
+		print pass_len
+		print slang
+		word_list = passphrase_functions.open_file(slang)
+		passphrase = passphrase_functions.gen_pw(int(pass_len), word_list)
+		flash(passphrase)
+		return redirect('/')
+	except KeyError:
+		pass_len = 3
+		slang = '415words.txt'
+		word_list = passphrase_functions.open_file(slang)
+		passphrase = passphrase_functions.gen_pw(int(pass_len), word_list)
+		flash(passphrase)
+		return redirect('/')
+
+@app.route("/upload", methods=["POST"])
+def upload():
+	pass
 
 app.secret_key = 'TheStick?Hubba#Penelopes$'
 
